53cc8afc8e4c138b214fa096e82d83f91849769a
Gederson <gedersongustavogbot@ufps.edu.co>
2023-02-01 17:42:48 -0500

    Services security implemented

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoginUser } from '../models/login-user';
+import { NewUser } from '../models/new-user';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
 
-  constructor() { }
+  AUTH_URI = 'http://localhost:8080/auth/'
+
+  constructor(private httpClient: HttpClient) { }
+
+  public new(newUser: NewUser): Observable<any> {
+    return this.httpClient.post<any>(this.AUTH_URI + 'new', newUser)
+  }
+
+  public login(loginUser: LoginUser): Observable<any> {
+    return this.httpClient.post<any>(this.AUTH_URI + 'login', loginUser)
+  }
 }
